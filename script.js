5fcf0efb24138b2c3638e8f9925d6c035903882d
Ivan Medvedovskyi <ivanmedvedovskiyq@gmail.com>
2023-04-28 16:44:02 +0300

    FinalVersion of Task23

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const imagesArray = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
 
-function generateImages(length, imagesArray) {
+function generateImages(imagesArray) {
     let generator = imagesArray[Math.floor(Math.random() * imagesArray.length)];
     const container = document.getElementById('container');
     container.innerHTML += '<img src="./images/' + generator + '">';
 }
 
-generateImages(imagesArray.length, imagesArray);
+generateImages(imagesArray);
 
 
